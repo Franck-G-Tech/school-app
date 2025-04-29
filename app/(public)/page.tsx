ec0235a32ac18432b9b2fc7362ef2d422d9b2a15
Jesus Francisco CAstrejón Canales <franck_768@hotmail.com>
2025-04-29 10:06:52 -0600

    cambio en la ubicacion de carpeta

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -5,10 +5,13 @@ import { TablaMaestros } from "@/components/tabla-maestros";
 export default function Home() {
   return (
     <main className="container mx-auto py-10">
+      <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', marginBottom: '20px' }}>
       <h1 className="text-3xl font-bold mb-6">Sistema de Estudiantes</h1>
       <TablaEstudiantes />
+      <br />
       <h1 className="text-3xl font-bold mb-6">Sistema de Maestros</h1>
       <TablaMaestros />
+      </div>
     </main>
   );
 }
--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,6 @@
 
 import { ConvexProvider } from "convex/react";
 import { convex } from "@/convex/client";
-//import { convex } from "@/convex/client";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return <ConvexProvider client={convex}>{children}</ConvexProvider>;
