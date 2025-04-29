18325f26aee6f759aa8fa004636dfe5c37926cc0
Jesus Francisco CAstrejón Canales <franck_768@hotmail.com>
2025-04-29 11:06:02 -0600

    test page admin

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -4,11 +4,11 @@ export default function Home() {
     <main className="container mx-auto py-10">
       <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', marginBottom: '20px' }}>
       <h1 className="text-3xl font-bold mb-6">Sistema de Estudiantes</h1>
-      <Link href={"/admin/"}>Sistema de Estudiantes</Link>
+      <Link href={"/admin/config"}>Sistema de Estudiantes</Link>
       <br />
       <br/>
       <h1 className="text-3xl font-bold mb-6">Sistema de Maestros</h1>
-      <Link href={"/admin/"}>Sistema de Maestros</Link>
+      <Link href={"/admin/config"}>Sistema de Maestros</Link>
       </div>
     </main>
   );
