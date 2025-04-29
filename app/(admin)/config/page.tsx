09c976e5e129223a062f120ff8de182b6e81d6dd
Jesus Francisco CAstrejón Canales <franck_768@hotmail.com>
2025-04-29 11:26:05 -0600

    test url page admin

--- app/(admin)/config/page.tsx
+++ app/(admin)/config/page.tsx
@@ -2,16 +2,16 @@
 import { TablaEstudiantes } from "@/components/tabla-estudiantes";
 import { TablaMaestros } from "@/components/tabla-maestros";
 
-export default function Home() {
+export default function ConfigPage() {
   return (
     <main className="container mx-auto py-10">
       <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', marginBottom: '20px' }}>
       <h1 className="text-3xl font-bold mb-6">Sistema de Estudiantes</h1>
-     <button> <TablaEstudiantes /></button>
+      <TablaEstudiantes />
       <br />
       <br/>
       <h1 className="text-3xl font-bold mb-6">Sistema de Maestros</h1>
-      <button><TablaMaestros /></button>
+      <TablaMaestros />
       </div>
     </main>
   );
--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,5 +1,5 @@
 import { Providers } from "../providers";
-import "./globals.css";
+import "@/app/styles/globals.css";
 //import "@/app/globals.css"
 
 export default function RootLayout({
