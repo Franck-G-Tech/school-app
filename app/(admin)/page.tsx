05263176f55281406256bf74e8b5120714835c57
Jesus Francisco CAstrejón Canales <franck_768@hotmail.com>
2025-04-29 10:29:36 -0600

    page public

--- app/(admin)/page.tsx
+++ app/(admin)/page.tsx
@@ -1,14 +1,17 @@
-import Link from "next/link";
+
+import { TablaEstudiantes } from "@/components/tabla-estudiantes";
+import { TablaMaestros } from "@/components/tabla-maestros";
+
 export default function Home() {
   return (
     <main className="container mx-auto py-10">
       <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', marginBottom: '20px' }}>
       <h1 className="text-3xl font-bold mb-6">Sistema de Estudiantes</h1>
-      <Link href={"/admin/"}>Sistema de Estudiantes</Link>
+      <TablaEstudiantes />
       <br />
       <br/>
       <h1 className="text-3xl font-bold mb-6">Sistema de Maestros</h1>
-      <Link href={"/admin/"}>Sistema de Maestros</Link>
+      <TablaMaestros />
       </div>
     </main>
   );
