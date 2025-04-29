11554cacf0d00d478b70a5519d208fce3c2eb47e
Jesus Francisco CAstrejón Canales <franck_768@hotmail.com>
2025-04-29 11:34:25 -0600

    br

--- app/(admin)/config/page.tsx
+++ app/(admin)/config/page.tsx
@@ -7,13 +7,13 @@ export default function ConfigPage() {
   return (
     <main className="container mx-auto py-10">
       <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', marginBottom: '20px' }}>
-      <h1 className="text-3xl font-bold mb-6">Sistema de Estudiantes</h1>
+      <h1 className="text-3xl font-bold mb-6">Sistema de Estudiantes</h1><br />
       <TablaEstudiantes />
       <br />
       <br/>
-      <h1 className="text-3xl font-bold mb-6">Sistema de Maestros</h1>
+      <h1 className="text-3xl font-bold mb-6">Sistema de Maestros</h1><br />
       <TablaMaestros />
-
+<br />
       <button><Link href="/">Ir a inicio</Link></button>
       </div>
     </main>
