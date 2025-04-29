db1fd69c641f594ac81445867a4561960aceb3d4
Jesus Francisco CAstrejón Canales <franck_768@hotmail.com>
2025-04-29 11:13:06 -0600

    test page admin

--- app/(admin)/admin.tsx
+++ app/(admin)/admin.tsx
@@ -1,17 +1,15 @@
 
-import { TablaEstudiantes } from "@/components/tabla-estudiantes";
-import { TablaMaestros } from "@/components/tabla-maestros";
 
 export default function Home() {
   return (
     <main className="container mx-auto py-10">
       <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', marginBottom: '20px' }}>
       <h1 className="text-3xl font-bold mb-6">admin folder</h1>
-     <button> <TablaEstudiantes /></button>
+     
       <br />
       <br/>
       <h1 className="text-3xl font-bold mb-6">admin folder de Maestros</h1>
-      <button><TablaMaestros /></button>
+     
       </div>
     </main>
   );
